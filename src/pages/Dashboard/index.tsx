c65da61eba53ebfe6df4fd7feb8a4191477d9ee4
Matheus Lino <7matheuslino@gmail.com>
2020-05-09 10:08:03 -0300

    Finishing class 'Estilizando Dashboard'

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title, Form, Repositories } from './styles'
+import logo from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
-   return <h1>Dashboard</h1>
-}
+   return (
+      <>
+         <img src={logo} alt="Github Explorer" />
+         <Title>Explore repositórios no GitHub</Title>
 
-export default Dashboard
+         <Form>
+            <input placeholder="Nome do repositório"/>
+            <button type="submit">Pesquisar</button>
+         </Form>
+
+         <Repositories>
+            <a href='teste'>
+               <img
+                  src='https://avatars1.githubusercontent.com/u/31441201?s=460&u=1cf93b13751255a1e6507b2e0f8faf1384e4294f&v=4'
+                  alt='Matheus Lino'
+               />
+
+               <div>
+                  <strong>7matheus/teste</strong>
+                  <p>Um teste apenas</p>
+               </div>
+
+               <FiChevronRight size={20}/>
+            </a>
+
+            <a href='teste'>
+               <img
+                  src='https://avatars1.githubusercontent.com/u/31441201?s=460&u=1cf93b13751255a1e6507b2e0f8faf1384e4294f&v=4'
+                  alt='Matheus Lino'
+               />
+
+               <div>
+                  <strong>7matheus/teste</strong>
+                  <p>Um teste apenas</p>
+               </div>
+
+               <FiChevronRight size={20}/>
+            </a>
+
+            <a href='teste'>
+               <img
+                  src='https://avatars1.githubusercontent.com/u/31441201?s=460&u=1cf93b13751255a1e6507b2e0f8faf1384e4294f&v=4'
+                  alt='Matheus Lino'
+               />
+
+               <div>
+                  <strong>7matheus/teste</strong>
+                  <p>Um teste apenas</p>
+               </div>
+
+               <FiChevronRight size={20}/>
+            </a>
+         </Repositories>
+      </>
+   )
+};
+
+export default Dashboard;
