e76c04be4e2680e699844655b3fa765384b2abcd
Matheus Lino <7matheuslino@gmail.com>
2020-05-09 21:44:28 -0300

    Finalizing class 'Navegando entre rotas'

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, FormEvent, useEffect } from 'react'
 import { FiChevronRight } from 'react-icons/fi';
-
-import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import { Title, Form, Repositories, Error } from './styles'
+import api from '../../services/api';
 import logo from '../../assets/logo.svg'
+
 
 interface Repository {
    full_name: string
@@ -72,7 +73,7 @@ const Dashboard: React.FC = () => {
 
          <Repositories>
             {repositories.map(repository => (
-               <a key={repository.full_name} href='teste'>
+               <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
                   <img
                      src={repository.owner.avatar_url}
                      alt={repository.owner.login}
@@ -84,7 +85,7 @@ const Dashboard: React.FC = () => {
                   </div>
 
                   <FiChevronRight size={20} />
-               </a>
+               </Link>
             ))}
          </Repositories>
       </>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,13 +2,13 @@ import styled, { css } from 'styled-components'
 import { shade } from "polished";
 
 interface FormProps {
-   hasError: boolean
-}
+   hasError: boolean;
+};
 
 export const Title = styled.h1`
    font-size: 48px;
    color: #3a3a3a;
-   max-width: 50px;
+   max-width: 450px;
    line-height: 56px;
 
    margin-top: 80px;
@@ -22,7 +22,7 @@ export const Error = styled.span`
 
 export const Form = styled.form<FormProps>`
    margin-top:40px;
-   max-width: 715;
+   max-width: 715px;
    display: flex;
 
    input {
